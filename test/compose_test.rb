6e6bca4d50cd60e6b9e834b515dd4154a9df68ec
Steven Davidovitz <sdavidovitz@zendesk.com>
2016-04-07 16:34:34 -0700

    add initial test framework

---
+++ test/compose_test.rb
@@ -0,0 +1,12 @@
+require_relative 'helper'
+
+describe Minitest::Docker::Compose do
+  it 'composes the environment' do
+  end
+
+  it 'waits for the environment to come up before continuing' do
+  end
+
+  it 'times out if the environment cannot come up' do
+  end
+end
